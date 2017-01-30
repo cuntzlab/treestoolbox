afb265e218f236da82c5471b0168998605724a19
Hermann Cuntz <treestoolbox@gmail.com>
2017-01-30 11:15:40 +0100

    ADDED: jitter on subset of nodes, by Marcel Beining

--- construct/jitter_tree.m
+++ construct/jitter_tree.m
@@ -1,8 +1,8 @@
 % JITTER_TREE   Jitters coordinates of a tree.
 % (trees package)
 %
-% tree = jitter_tree (intree, stde, lambda, options)
-% --------------------------------------------------
+% tree = jitter_tree (intree, stde, lambda, options, inodes)
+% ----------------------------------------------------------
 %
 % adds spatial noise to the coordinates of the nodes of a tree.
 %
@@ -18,6 +18,7 @@
 %     '-s'   : show
 %     '-w'   : waitbar
 %     {DEFAULT: '-w'}
+% - inodes   ::index: nodes of the tree affected by jitter
 %
 % Output
 % ------
@@ -31,12 +32,13 @@
 % See also smooth_tree MST_tree
 % Uses ipar_tree
 %
-% speed up suggested by Calvin Schneider 2014
+% speed up suggested  by Calvin Schneider 2014
+% added inodes option by Marcel Beining   2015
 %
 % the TREES toolbox: edit, generate, visualise and analyse neuronal trees
 % Copyright (C) 2009 - 2016  Hermann Cuntz
 
-function  varargout = jitter_tree (intree, stde, lambda, options)
+function  varargout = jitter_tree (intree, stde, lambda, options, inodes)
 
 % trees : contains the tree structures in the trees package
 global trees
@@ -66,76 +68,85 @@ if (nargin < 4) || isempty (options)
 end
 
 N            = size (tree.X, 1);
-% all paths:
-A            = tree.dA + tree.dA';
-As           = cell (1, 1);
 
-if strfind   (options, '-w')     % waitbar option: initialization
-    HW       = waitbar (0, 'calculating paths...');
-    set      (HW, ...
+if (nargin < 5) || isempty (inodes)
+    inodes   = 1 : N;
+end
+if islogical (inodes) && numel (inodes) == N  % transform logical indexing
+    inodes   = find (inodes);
+end
+
+% all paths:
+A                = tree.dA + tree.dA';
+As               = cell (1, 1);
+
+if strfind       (options, '-w')     % waitbar option: initialization
+    HW           = waitbar (0, 'calculating paths...');
+    set          (HW, ...
         'Name',                  '..PLEASE..WAIT..YEAH..');
 end
 
-for ward     = 1 : lambda
-    if strfind (options, '-w')   % waitbar option: update
-        if mod (ward, 5) == 0
-            waitbar (ward / lambda, HW);
+for counter      = 1 : lambda
+    if strfind   (options, '-w')   % waitbar option: update
+        if mod (counter, 5) == 0
+            waitbar (counter / lambda, HW);
         end
     end
-    As {ward} = A ^ ward;
+    As{counter}  = A ^ counter;
 end
-R            = randn (N, 3) * stde * lambda;
-R1           = zeros (N, 3);
+R                = zeros (N, 3);
+R (inodes, :)    = randn (numel (inodes), 3) * stde * lambda;
+R1               = zeros (N, 3);
 
-if strfind   (options, '-w')     % waitbar option: reinitialization
-    waitbar  (0, ...
+if strfind       (options, '-w')     % waitbar option: reinitialization
+    waitbar      (0, ...
         HW,                      'jittering...');
 end
 
-for ward     = 1 : N
-    if strfind (options, '-w')   % waitbar option: update
-        if mod (ward, 50) == 0,
-            waitbar (ward / N, HW);
+for counter      = 1 : N
+    if strfind   (options, '-w')   % waitbar option: update
+        if mod (counter, 50) == 0,
+            waitbar (counter / N, HW);
         end
     end
-    Z        = zeros (N, 1);
-    Z (ward) = 1;
-    S        = zeros (N, 1);
-    S1       = zeros (N, 1);
-    for te   = 1 : lambda
-        zA   = As{te} * Z > 0;
-        iA   = find ((zA - S1) > 0);
-        S1 (iA) =  1;
-        S  (iA) = te;
+    Z            = zeros (N, 1);
+    Z (counter)  = 1;
+    S            = zeros (N, 1);
+    S1           = zeros (N, 1);
+    for counter2 = 1 : lambda
+        zA       = As{counter2} * Z > 0;
+        iA       = find ((zA - S1) > 0);
+        S1 (iA)  =  1;
+        S  (iA)  = counter2;
     end
     S (S == 0)   = 100000;
     S            = gauss (S, 1, lambda / 5);
-    R1 (ward, :) = sum   (R .* [S S S]);
+    R1 (counter, :) = sum   (R .* [S S S]);
 end
 
-if strfind   (options, '-w')       % waitbar option: close
-    close    (HW);
+if strfind       (options, '-w')       % waitbar option: close
+    close        (HW);
 end
 
-tree.X       = tree.X + R1 (:, 1) - R1 (1, 1);
-tree.Y       = tree.Y + R1 (:, 2) - R1 (1, 2);
-tree.Z       = tree.Z + R1 (:, 3) - R1 (1, 3);
+tree.X           = tree.X + R1 (:, 1) - R1 (1, 1);
+tree.Y           = tree.Y + R1 (:, 2) - R1 (1, 2);
+tree.Z           = tree.Z + R1 (:, 3) - R1 (1, 3);
 
-if strfind   (options, '-s')
+if strfind       (options, '-s')
     clf; hold on;
-    plot_tree (intree);
-    plot_tree (tree, [1 0 0]);
-    HP (1)   = plot (1, 1, 'k-');
-    HP (2)   = plot (1, 1, 'r-');
-    legend   (HP, {'before', 'after'});
-    set      (HP, 'visible', 'off');
-    title    ('jitter tree');
-    xlabel   ('x [\mum]');
-    ylabel   ('y [\mum]');
-    zlabel   ('z [\mum]');
-    view     (2);
-    grid     on;
-    axis     image;
+    plot_tree    (intree);
+    plot_tree    (tree, [1 0 0]);
+    HP (1)       = plot (1, 1, 'k-');
+    HP (2)       = plot (1, 1, 'r-');
+    legend       (HP, {'before', 'after'});
+    set          (HP, 'visible', 'off');
+    title        ('jitter tree');
+    xlabel       ('x [\mum]');
+    ylabel       ('y [\mum]');
+    zlabel       ('z [\mum]');
+    view         (2);
+    grid         on;
+    axis         image;
 end
 
 if (nargout == 1) || (isstruct (intree))
