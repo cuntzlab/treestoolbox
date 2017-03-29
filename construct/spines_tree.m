09ff613cde99a6888cb8fc4ca3c3cb94408b548f
Hermann Cuntz <treestoolbox@gmail.com>
2017-03-29 11:21:50 +0200

    huge improvement of the function by Marcel Beining

--- construct/spines_tree.m
+++ construct/spines_tree.m
@@ -1,7 +1,8 @@
 % SPINES_TREE   Add spines to an existing tree.
 % (trees package)
 %
-% tree = spines_tree (intree, XYZ, dneck, dhead, mlneck, stdlneck, ipart,
+% [tree, indhead, indneck] = spines_tree (intree, XYZ, ...
+% dneck, dhead, mlneck, stdlneck, ipart,
 % options)
 % -------------------------------------------------------------------------
 %
@@ -15,7 +16,7 @@
 % Input
 % -----
 % - intree   ::integer: index of tree in trees or structured tree
-% - XYZ      :: matrix: [X Y Z] or just a number of spines to add
+% - XYZ      ::matrix: [X Y Z] or just a number of spines to add
 %     {DEFAULT: 100}
 % - dneck    ::value:   diameter of spine neck
 %     {DEFAULT: 0.25 um}
@@ -31,24 +32,30 @@
 % - options  ::string:
 %     '-s'   : show
 %     '-w'   : waitbar
+%     '-sr': instead of just adding a region "spines" it adds region for
+%            spine heads and necks
 %     {DEFAULT '-w'}
 %
 % Output
 % ------
 % if no output is declared the trees are added in trees
 % - tree     :: structured output tree
+% - indhead  :: node indices of spine heads
+% - indneck  :: node indices of spine necks
 %
 % Example
 % -------
 % spines_tree  (sample_tree, 300, 0.25, 1, 1, 1, [], '-s')
 %
-% See also quaddiameter_tree MST_tree
+% See also resample_tree quaddiameter_tree MST_tree
 % Uses 
 %
+% Massive improvement by Marcel Beining 2017
+%
 % the TREES toolbox: edit, generate, visualise and analyse neuronal trees
-% Copyright (C) 2009 - 2016  Hermann Cuntz
+% Copyright (C) 2009 - 2017  Hermann Cuntz
 
-function varargout = spines_tree (intree, XYZ, ...
+function [tree, indhead, indneck] = spines_tree (intree, XYZ, ...
     dneck, dhead, mlneck, stdlneck, ...
     ipart, options)
 
@@ -72,16 +79,7 @@ end
 X            = tree.X;       % X-locations of nodes on tree
 Y            = tree.Y;       % Y-locations of nodes on tree
 Z            = tree.Z;       % Z-locations of nodes on tree
-
 N            = size (X, 1);  % number of nodes in tree
-
-if (nargin < 5) || isempty (mlneck)
-    mlneck   = 1;
-end
-
-if (nargin < 6) || isempty (stdlneck)
-    stdlneck = 1;
-end
 
 if (nargin < 2) || isempty (XYZ)
     XYZ      = 100;
@@ -95,79 +93,112 @@ if (nargin < 4) || isempty (dhead)
     dhead    = 1;
 end
 
+if (nargin < 5) || isempty (mlneck)
+    mlneck   = 1;
+end
+
+if (nargin < 6) || isempty (stdlneck)
+    stdlneck = 1;
+end
+
 if (nargin < 7) || isempty (ipart)
     % {DEFAULT index: select all nodes/points}
     ipart    = (1 : N)';
 end
 
-if numel (XYZ) == 1
-    indy     = ceil (rand (XYZ, 1) * length (ipart));
-    XYZ      = [ ...
-        (X (ipart (indy))) ...
-        (Y (ipart (indy))) ...
-        (Z (ipart (indy)))] + ...
-        (randn (XYZ, 3) * stdlneck + mlneck) .* ...
-        ((rand (XYZ, 3) > 0.5) * 2 - 1);
+if (nargin < 8) || isempty (options)
+    options  = '-w';
 end
+
+direction        = direction_tree (tree, '-n');
+if     numel (XYZ) == 1
+    indy         = ceil (rand (XYZ, 1) * length (ipart));
+elseif all   (XYZ < N) % they are indices
+    indy         = XYZ;
+end
+dXYZ             = zeros (numel (indy), 3);
+for counter      = 1 : numel (indy)
+    % calculate an orthogonal vector to the direction of the dendrite:
+    [~, ~, V]    = svd (direction (ipart (indy (counter)), :), 0);  
+    % rotate the vector around axis dendrite axis by random radians:
+    M            = makehgtform ('axisrotate', ....
+        direction (ipart (indy (counter)), :), ...
+        2 * rand () * pi);
+    dXYZ (counter, :) = V (:, 3)' * M (1 : 3, 1 : 3);
+end
+XYZ              = [ ...
+    (X (ipart (indy))) ...
+    (Y (ipart (indy))) ...
+    (Z (ipart (indy)))] + ...
+    repmat ((randn (numel (indy), 1) * stdlneck + mlneck), 1, 3) .* dXYZ;
 
 if isfield       (tree, 'R')
     if isfield   (tree, 'rnames')
-        iR       = find (strcmp (tree.rnames, 'spines'));
-        if       ~isempty (iR)
-            iR   = iR (1);
+        if ~isempty (strfind (options, '-sr'))
+            r    = find (strcmpi (tree.rnames, 'spine_neck'));
+            if ~isempty (r)
+                iR (1) = r (1);
+            else
+                iR (1) = max (max (tree.R), numel (tree.rnames)) + 1;
+                flag   = 1;
+            end
+            r          = find (strcmpi (tree.rnames, 'spine_head'));
+            if ~isempty (r)
+                iR (2) = r (1);
+            else
+                iR (2) = max (max (tree.R), numel (tree.rnames)) + 1 + flag;
+            end
         else
-            iR   = max (tree.R) + 1;
+            iR         = find (strcmp (tree.rnames, 'spines'));
+            if ~isempty (iR)
+                iR     = iR (1);
+            else
+                iR     = max (tree.R,numel (tree.rnames)) + 1;
+                tree.rnames = [tree.rnames, 'spines'];
+            end
         end
     else
-        iR       = max (tree.R) + 1;
+        iR       = max (tree.R,numel (tree.rnames)) + 1;
     end
 else
     iR           = 1;
 end
 
-if (nargin < 8) || isempty (options)
-    options      = '-w';
+if numel (iR)    == 1
+    iR (2)       = iR (1);
 end
-if strfind       (options, '-w')    % waitbar option: initialization
+if ~isempty (strfind (options, '-sr'))
+    tree.rnames{iR(1)} = 'spines_neck';
+    tree.rnames{iR(2)} = 'spines_head';
+end
+
+if strfind (options, '-w') % waitbar option: initialization
     if size (XYZ, 1) > 99
-        HW       = waitbar (0, 'spining...');
-        set      (HW, 'Name', '..PLEASE..WAIT..YEAH..');
+        HW = waitbar (0, 'spining...');
+        set (HW, 'Name', '..PLEASE..WAIT..YEAH..');
     end
 end
-for counter      = 1 : size (XYZ, 1)
-    if strfind   (options, '-w')    % waitbar option: update
-        if mod   (counter, 100) == 0
+for counter = 1 : size (XYZ, 1)
+    if strfind (options, '-w') % waitbar option: update
+        if mod (counter, 100) == 0
             waitbar (counter / size (XYZ, 1), HW);
         end
     end
-    [~, i2]      = min (sqrt ( ...
-        (X (ipart) - XYZ (counter, 1)).^2 + ...
-        (Y (ipart) - XYZ (counter, 2)).^2 + ...
-        (Z (ipart) - XYZ (counter, 3)).^2));
-    dXYZ         = XYZ (counter, :) - ...
-        [(X (ipart (i2))) (Y (ipart (i2))) (Z (ipart (i2)))];
-    dXYZ         = dXYZ ./ norm(dXYZ);
-    tree         = insert_tree (tree, ...
-        [1 iR ...
+    [tree, indneck]    = insert_tree (tree, ...
+        [1 (iR (1)) ...
         (XYZ (counter, 1)) ...
         (XYZ (counter, 2)) ...
         (XYZ (counter, 3)) ...
-        dneck (ipart (i2))], 'none');
-    tree         = insert_tree (tree, ...
-        [1 iR ...
-        (XYZ (counter, 1) + dXYZ (1)), ...
-        (XYZ (counter, 2) + dXYZ (2)) ...
-        (XYZ (counter, 3) + dXYZ (3)) ...
+        dneck (ipart (indy (counter)))], 'none');
+    [tree, indhead]    = insert_tree (tree, ...
+        [1 (iR (2)) ...
+        (XYZ (counter, 1) + dXYZ (counter, 1) * dhead) ...
+        (XYZ (counter, 2) + dXYZ (counter, 2) * dhead) ...
+        (XYZ (counter, 3) + dXYZ (counter, 3) * dhead) ...
         dhead (N + 1 + 2 * (counter - 1))], 'none');
 end
-if strfind       (options, '-w') % waitbar option: close
-    if size (XYZ, 1) > 99
-        close    (HW);
-    end
-end
-
-if isfield (tree, 'rnames') && isfield (tree, 'R')
-    tree.rnames{end} = 'spines';
+if strfind (options, '-w') % waitbar option: close
+    close (HW);
 end
 
 if strfind       (options, '-s')
@@ -182,9 +213,10 @@ if strfind       (options, '-s')
     axis         image;
 end
 
-if (nargout == 1) || (isstruct (intree))
-    varargout{1}  = tree; % if output is defined then it becomes the tree
-else
-    trees{intree} = tree; % otherwise the orginal tree in trees is replaced
+if (nargout == 0) && (~isstruct (intree))
+   trees{intree} = tree; % otherwise the orginal tree in trees is replaced
 end
+
+
+
 
