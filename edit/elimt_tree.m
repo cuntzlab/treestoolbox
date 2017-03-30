6fbda999e0ffdf654cc3d1775db60ebf1eb8be0e
Hermann Cuntz <treestoolbox@gmail.com>
2017-03-30 11:30:52 +0200

    elimt_tree can give an output how many trifurcations and other amendments by Marcel Beining

--- edit/elimt_tree.m
+++ edit/elimt_tree.m
@@ -1,26 +1,27 @@
 % ELIMT_TREE   Replace multifurcations by multiple bifurcations in a tree.
 % (trees package)
-% 
-% tree = elimt_tree (intree, options)
-% -----------------------------------
-% 
+%
+% [tree, itrif] = elimt_tree (intree, options)
+% --------------------------------------------
+%
 % Eliminates the trifurcations/multifurcations present in the tree's
-% adjacency matrix by adding tiny (x-deflected) compartments. The node
-% labels are messed up and should be sorted after application of this
-% function. This function alters the original morphology minimally!
-% 
+% adjacency matrix by adding tiny (x-deflected) compartments.
+% This function alters the original morphology minimally!
+%
 % Input
 % -----
 % - intree   ::integer:   index of tree in trees or structured tree
 % - options  ::string:
 %     '-s'   : show
-%     '-e'   : echo added nodes
+%     '-0'   : do not eliminate trifurcation at root
+%     '-e'   : echo changes
 %     {DEFAULT: ''}
 %
 % Output
 % ------
 % if no output is declared the tree is changed in trees
 % - tree     :: structured output tree
+% - itrif    :: indices of trifurcations
 %
 % Example
 % -------
@@ -31,7 +32,7 @@
 % Uses ver_tree dA
 %
 % the TREES toolbox: edit, generate, visualise and analyse neuronal trees
-% Copyright (C) 2009 - 2016  Hermann Cuntz
+% Copyright (C) 2009 - 2017  Hermann Cuntz
 
 function varargout = elimt_tree (intree, options)
 
@@ -57,31 +58,65 @@ if (nargin < 2) || isempty (options)
     options  = '';
 end
 
-dA           = tree.dA;            % directed adjacency matrix of tree
-num          = size (dA, 1);       % number of nodes in tree
-sumdA        = ones (1, num) * dA; % typeN: (actually faster than sum (dA))
-itrif        = find (sumdA > 2);   % find trifurcations
+dA               = tree.dA;            % directed adjacency matrix of tree
+num              = size (dA, 1);       % number of nodes in tree
+sumdA            = ones (1, num) * dA; % actually faster than sum (dA)!
+idpar            = idpar_tree (tree);
+itrif            = find (sumdA > 2);   % find trifurcations
+if strfind (options, '-0')             % do not eliminate root trifurcation
+    itrif        = setdiff (itrif, find ((dA * ones (num, 1)) == 0));
+end
 
 for counter      = 1 : length (itrif)
-    N            = size  (dA, 1);
+    N            = size (dA, 1);
     fed          = sumdA (itrif (counter)) - 2;
     dA           = [[dA; (zeros (fed, num))], (zeros (fed + num, fed))];
-    % lengthen all vectors of form Nx1
+    % lengthen all vectors of form Nx1:
     S            = fieldnames (tree);
-    for counterS       = 1 : length (S)
+    dX           = ...
+        tree.X (itrif (counter)) - ...
+        tree.X (idpar (itrif (counter)));
+    dY           = ...
+        tree.Y (itrif (counter)) - ...
+        tree.Y (idpar (itrif (counter)));
+    dZ           = ...
+        tree.Z (itrif (counter)) - ...
+        tree.Z (idpar (itrif (counter)));
+    if all       ([dX, dY, dZ] == 0)
+        dX        = mean (tree.X) - tree.X (1);
+        dY        = mean (tree.Y) - tree.Y (1);
+        dZ        = mean (tree.Z) - tree.Z (1);
+    end
+    normvec      = norm ([dX, dY, dZ]);
+    dX           = dX / normvec;
+    dY           = dY / normvec;
+    dZ           = dZ / normvec;
+    for counterS = 1 : length (S)
         if ~strcmp (S{counterS}, 'dA')
             vec  = tree.(S{counterS});
-            if isvector (vec) && (numel (vec) == N) && ~(ischar (vec))
-                if strcmp  (S{counterS}, 'X'),
+            if isvector (vec) && isnumeric (vec) && (numel (vec) == N)
+                if strcmp (S{counterS},'X')
                     tree.X = [tree.X; ...
                         (ones (fed, 1) .* ...
                         tree.X (itrif (counter))) + ...
-                        (0.0001 * (1 : fed)')];
+                        (0.0001 * dX .* (1 : fed)')];
+                elseif strcmp (S{counterS}, 'Y')
+                    tree.Y = [tree.Y; ...
+                        (ones (fed, 1) .* ...
+                        tree.Y (itrif (counter))) + ...
+                        (0.0001 * dY .* (1 : fed)')];
+                elseif strcmp (S{counterS}, 'Z')
+                    tree.Z = [tree.Z; ...
+                        (ones (fed, 1) .* ...
+                        tree.Z (itrif (counter))) + ...
+                        (0.0001 * dZ .* (1 : fed)')];
                 else
                     tree.(S{counterS}) = [tree.(S{counterS}); ...
-                        (ones (fed, 1) .* ...
-                        tree.(S{counterS})(itrif (counter)))];
+                        ones(fed,1).*tree.(S{counterS})(itrif(counter))];
                 end
+            elseif iscell (vec) && (numel (vec) == N)
+                tree.(S{counterS}) = [tree.(S{counterS}); ...
+                    (repmat (tree.(S{counterS}) (itrif (counter)), fed, 1))];
             end
         end
     end
@@ -92,7 +127,7 @@ for counter      = 1 : length (itrif)
     dA (ibs (2), num)             = 1;
     for counterdA  = 3 : sumdA (itrif (counter)) - 1
         num      = num + 1;
-        dA (num, num - 1)                   = 1;
+        dA (num, num - 1)                     = 1;
         dA (ibs (counterdA), itrif (counter)) = 0;
         dA (ibs (counterdA), num)             = 1;
     end
@@ -116,7 +151,7 @@ if strfind       (options, '-s')   % show option
     axis         image;
 end
 
-if strfind       (options,'-e')
+if strfind       (options, '-e')
     display      ([...
         'elimt_tree: eliminated ' ...
         (num2str (length (itrif))) ...
@@ -128,4 +163,6 @@ if (nargout == 1) || (isstruct (intree))
 else
     trees{intree} = tree;
 end
-
+if (nargout >= 2)
+    varargout{2}  = ~isempty (itrif);
+end
