ebbe32f005c6b8ff834ccbf23d4c28dfb2be5b7f
Hermann Cuntz <treestoolbox@gmail.com>
2017-04-05 14:26:01 +0200

    updated testing functions

--- tests/construct/check_cap_tree.m
+++ tests/construct/check_cap_tree.m
@@ -2,8 +2,8 @@
 
 %% test 1
 tree         = resample_tree (sample_tree, 1, 'none');
-stree        = soma_tree    (tree, 20, 30, 'none'); % WRONG MEASURES!!!!
-ctree        = cap_tree     (stree, '-i1 -s'); % WRONG MEASURES!!!!
+stree        = soma_tree    (tree, 20, 30, 'none');
+ctree        = cap_tree     (stree, '-i1 -s');
 tprint       ('./panels/cap_tree1', ...
     '-jpg -HR',                [10 10]);
 
