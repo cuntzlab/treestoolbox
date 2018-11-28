0d7f8dc7490cb09c243c878fe87075be61bcd8cb
Hermann Cuntz <hermann.neuro@gmail.com>
2018-11-28 20:55:52 +0100

    Adding a new regularity index from our new paper Anton-Sanchez et al 2018 PLoS Comp Biol

---
+++ construct/PP_generator_tree.m
@@ -0,0 +1,393 @@
+% PP_GENERATOR_TREE   Distributes points with given R value.
+% (trees package)
+%
+% [P, numIt, Rvalues] = PP_generator_tree ( ...
+%   N, R, a, alpha, n_mc, level, epsilon, options)
+% -------------------------------------------------------------
+%
+% Distributes N random points in a 200 x 200 um2 square or in a
+% 200 x 200 x 200 um3 cubus with a given R value that determines
+% whether points are clustered ( R < 1 ), Poisson distributed ( R = 1 )
+% or regularly distributed ( R > 1 ). If N is an Nx3 matrix it is
+% considered starting coordinates that will be transformed into a
+% distribution with the given R value.
+%
+% The function implements a exclusion zone around the target 
+% points due to, for example, the physical dimension of synapses 
+% that these might represent.
+%
+% Inputs
+% ------
+% - N        ::integer    : number of points to be distributed or Nx3 xyz
+%      	coordinates
+% - R        ::value	  : R value of the target point cloud
+% - a        ::value	  : controls the size of the movements of the
+%		points when looking for the given R value. Must be nonzero
+%     	{DEFAULT: 0.1}
+% - alpha    ::value	  : shrink factor used to obtain the volume
+%		supporting a given point cloud. alpha is a scalar between 0 and 1.
+%		Setting alpha to 0 gives the convex hull, and setting alpha to 1
+%		gives a compact boundary that envelops the points
+%		(for r_mc_tree function)
+%     	{DEFAULT: 0.5}
+% - n_mc     :: integer   : maximum number of Monte Carlo iterations
+%		(for r_mc_tree function)
+%     	{DEFAULT: 100}
+% - level    ::value	  : confidence intervals are obtained with
+%		confidence level (1 - level)
+%		(for r_mc_tree function)
+%     	{DEFAULT: 0.05}
+% - epsilon  :: minimum distance between points 
+%     	{DEFAULT: 0}
+% - options  ::string:
+%     	'-m' : show the point cloud in each iteration
+%		'-s' : show the resulting point cloud
+%	  	'-3d' : 3D point cloud
+%     	{DEFAULT: '-2d -m'}
+%
+% Output
+% ------
+% - P :: Nx2 matrix with xy coordinates (2D) or Nx3 matrix with xyz
+%       coordinates (3D)
+%		of the points with the given R value
+% - numIt :: number of iterations needed to obtain the given R value
+% - Rvalues :: R values in all iterations
+%
+% Example
+% -------
+% P = PP_generator_tree (100, 1.2);
+%
+% See also r_mc_tree
+% Uses
+%
+% Contributed by Laura Anton
+%
+% the TREES toolbox: edit, generate, visualise and analyse neuronal trees
+% Copyright (C) 2009 - 2018  Hermann Cuntz
+
+function [P, numIt, Rvalues] = PP_generator_tree ( ...
+    N, R, a, alpha, n_mc, level, epsilon, options)
+
+if nargin    < 1 || isempty (N)
+    N        = 100;
+end
+
+if nargin    < 2 || isempty (R)
+    R        = 1.2;
+end
+
+if nargin    < 3 || isempty (a)
+    a        = 0.1;
+end
+
+if nargin    < 4 || isempty (alpha)
+    alpha    = 0.5;
+end
+
+if nargin    < 5 || isempty (n_mc)
+    n_mc     = 100;
+end
+
+if nargin    < 6 || isempty (level)
+    level    = 0.05;
+end
+
+if nargin    < 7 || isempty (epsilon)
+    epsilon  = 0;
+end
+
+if nargin    < 8 || isempty (options)
+    options  = '-2d -m';
+end
+
+if (epsilon > 0) % volume exclusion (epsilon is the minimum distance between points)
+	X                    = [0];
+	Y                    = [0];
+	if strfind           (options, '-3d')
+		Z                = [0];
+	end
+	counter              = 1;
+	while                (counter < N)
+		xini             = ceil (rand (1, 1) * 200 - 100);
+		yini             = ceil (rand (1, 1) * 200 - 100);
+		if strfind       (options, '-3d')
+			zini         = ceil (rand (1, 1) * 200 - 100);
+			distance     = sqrt ( ...
+				(xini - X).^2 + ...
+				(yini - Y).^2 + ...
+				(zini - Z).^2);
+		else
+			distance     = sqrt ( ...
+				(xini - X).^2 + ...
+				(yini - Y).^2);
+		end
+		if (sum (distance < epsilon) == 0) % no intersections
+			X            = [X; xini];
+			Y            = [Y; yini];
+			if strfind   (options, '-3d')
+				Z        = [Z; zini];
+			end
+			counter      = counter + 1;
+		end
+	end
+
+	if strfind           (options, '-3d')
+		P                = [X, Y, Z];
+		t                = struct( ...
+			'X', X, ...
+			'Y', Y, ...
+			'Z', Z);
+		Ract             = r_mc_tree (t, alpha, n_mc, level, '-3d');
+	else
+		P                = [X, Y];
+		t                = struct ( ...
+			'X', X, ...
+			'Y', Y, ...
+			'Z', 0 * X);
+		Ract             = r_mc_tree (t, alpha, n_mc, level, '-2d');
+	end
+	Rvalues              = Ract; % initial R value
+
+	if strfind           (options, '-m')
+		clf;
+		if strfind       (options, '-3d')
+			plot3        (X, Y, Z, 'k.');
+            view         (3);
+		else
+			plot         (X, Y, 'k.');
+		end
+		axis             equal off;
+		title            (Ract);
+		drawnow;
+	end
+
+	if ( ((Ract > R) && (a > 0)) || ((Ract < R) && (a < 0)))
+		a                = -a;
+	end
+
+	numIt                = 0;
+	while ((Ract < R - 0.01) || (Ract > R + 0.01))
+		disp             ([Ract R]);
+		iNN              = zeros (N, 1);
+		dR               = abs   (R - Ract);
+		for counterNN    = 1 : N
+			if strfind   (options, '-3d')
+				d        = sqrt ( ...
+					(X (counterNN) - X).^2 + ...
+					(Y (counterNN) - Y).^2 + ...
+					(Z (counterNN) - Z).^2);
+			else
+				d        = sqrt ( ...
+					(X (counterNN) - X).^2 + ...
+					(Y (counterNN) - Y).^2);
+			end
+			d (counterNN) = inf;
+			movedPoint   = 0;
+			neighbors    = N - 1;
+			while ((movedPoint == 0) && (neighbors > 0))
+				
+				[~, i1]  = min (d);
+				iNN (counterNN) = i1;
+				dX       = (X (counterNN) - X (iNN (counterNN)));
+				dY       = (Y (counterNN) - Y (iNN (counterNN)));
+				newx     = X (counterNN) + dR * a * dX;
+				newy     = Y (counterNN) + dR * a * dY;
+				if (newx < -100)
+					newx = -100;
+				end
+				if (newx > 100)
+					newx = 100;
+				end
+				if (newy < -100)
+					newy = -100;
+				end
+				if (newy > 100)
+					newy = 100;
+				end
+				if strfind (options, '-3d')
+					dZ   = (Z (counterNN) - Z (iNN (counterNN)));
+					newz = Z (counterNN) + dR * a * dZ;
+					if (newz < -100)
+						newz = -100;
+					end
+					if (newz > 100)
+						newz = 100;
+					end
+					distance = sqrt ( ...
+						(newx - X).^2 + ...
+						(Y (counterNN) - Y).^2 + ...
+						(Z (counterNN) - Z).^2);
+				else
+					distance = sqrt ( ...
+						(newx - X).^2 + ...
+						(Y (counterNN) - Y).^2);
+				end
+				distance (counterNN) = inf;
+				if (sum(distance < epsilon) == 0) % no intersections
+					X (counterNN) = newx;
+					Y (counterNN) = newy;
+					if strfind (options, '-3d')
+						Z (counterNN) = newz;
+					end
+					movedPoint = 1;
+				else
+					% we can't move the point in the direction of this
+					% neighbor:
+					d (i1) = inf;
+					neighbors = neighbors - 1;
+				end
+			end
+        end
+        
+		if strfind       (options, '-3d')
+			P            = [X, Y, Z];
+			t            = struct ( ...
+				'X', X, ...
+				'Y', Y, ...
+				'Z', Z);
+			Ract         = r_mc_tree (t, alpha, n_mc, level, '-3d');
+		else
+			P            = [X, Y];
+			t            = struct ( ...
+				'X', X, ...
+				'Y', Y, ...
+				'Z', 0 * X);
+			Ract         = r_mc_tree (t, alpha, n_mc, level, '-2d');
+		end
+		Rvalues          = [Rvalues, Ract];
+		
+		if (((Ract > R) && (a > 0)) || ((Ract < R) && (a < 0)))
+			a            = -a;
+		end
+		
+		if strfind       (options, '-m')
+			clf; hold    on;
+			if strfind   (options, '-3d')
+				plot3    (X, Y, Z, 'k.');
+                view     (3)
+			else
+				plot     (X, Y, 'k.');
+			end
+			axis         equal off;
+			title        (Ract);
+			drawnow;
+		end
+		numIt            = numIt + 1;
+	end
+	
+else % no volume exclusion
+	xini                 = ceil (rand ((N - 1), 1) * 200 - 100);
+	yini                 = ceil (rand ((N - 1), 1) * 200 - 100);
+	X                    = [0; xini];
+	Y                    = [0; yini];
+	if strfind           (options, '-3d')
+		zini             = ceil (rand ((N - 1), 1) * 200 - 100);
+		Z                = [0; zini];
+		P                = [X, Y, Z];
+		t                = struct ('X', X, 'Y', Y, 'Z', Z);
+		Ract             = r_mc_tree (t, alpha, n_mc, level, '-3d'); 
+	else
+		P                = [X, Y];
+		t                = struct ('X', X, 'Y', Y, 'Z', 0*X);
+		Ract             = r_mc_tree (t, alpha, n_mc, level, '-2d'); 
+	end
+	Rvalues              = Ract; % initial R value
+
+	if strfind           (options, '-m')
+		clf;
+		if strfind       (options, '-3d')
+			plot3        (X, Y, Z, 'k.');
+            view         (3);
+		else
+			plot         (X, Y, 'k.'); 
+		end
+		axis             equal off;
+		title            (Ract);
+		drawnow;
+	end
+
+	if (((Ract > R) && (a > 0)) || ((Ract < R) && (a < 0)))
+		a                = -a;
+	end
+
+	numIt                = 0;
+	while ((Ract < R - 0.01) || (Ract > R + 0.01))
+		disp             ([Ract R]);
+		iNN              = zeros (N, 1);
+		dR               = abs   (R - Ract);
+		for counterNN    = 1 : N
+			if strfind   (options, '-3d')
+				d        = sqrt ( ...
+                    (X (counterNN) - X).^2 + ...
+                    (Y (counterNN) - Y).^2 + ...
+                    (Z (counterNN) - Z).^2);
+			else
+				d        = sqrt ( ...
+                    (X (counterNN) - X).^2 + ...
+                    (Y (counterNN) - Y).^2);
+			end
+			d (counterNN) = inf;
+			[~, i1]      = min (d);
+			iNN (counterNN) = i1;
+		end
+		dX               = (X - X (iNN));
+		dY               = (Y - Y (iNN));
+		X                = X + dR * a * dX;
+		Y                = Y + dR * a * dY;
+		X (X < -100)     = -100;
+		Y (Y < -100)     = -100;
+		X (X >  100)     =  100;
+		Y (Y >  100)     =  100;
+		
+		if strfind       (options, '-3d')
+			dZ           = (Z - Z (iNN));
+			Z            = Z + dR * a * dZ;
+			Z (Z < -100) = -100;
+			Z (Z >  100) =  100;
+			P            = [X, Y, Z];
+			t            = struct ('X', X, 'Y', Y, 'Z', Z);
+			Ract         = r_mc_tree (t, alpha, n_mc, level, '-3d');
+		else
+			P            = [X, Y];
+			t            = struct ('X', X, 'Y', Y, 'Z', 0 * X);
+			Ract         = r_mc_tree (t, alpha, n_mc, level, '-2d');
+		end
+		Rvalues = [Rvalues, Ract];
+		
+		if ( ((Ract > R) && (a > 0)) || ((Ract < R) && (a < 0)))
+			a = -a;
+		end
+
+		if strfind (options, '-m')
+			clf;
+			if strfind (options, '-3d')
+				plot3 (X, Y, Z, 'k.');
+                view (3);
+			else
+				plot (X, Y, 'k.'); 
+			end
+			axis         equal off;
+			title        (Ract);
+			drawnow;
+		end
+		numIt = numIt + 1;
+		
+	end 
+
+end
+
+if strfind       (options, '-s')
+    clf;
+    hold         on;
+    if strfind   (options, '-3d')
+        plot3    (X, Y, Z, 'k.');
+    else
+        plot     (X, Y, 'k.');
+    end
+    axis         equal off;
+    title        (Ract);
+    drawnow;
+end
+end
+
+
---
+++ metrics/r_mc_tree.m
@@ -0,0 +1,217 @@
+% R_MC_TREE   Checks R value in given tree.
+% (trees package)
+%
+% [R, Rmin, Rmax, r0, rE, rEmin, rEmax, rEstd, n, rEs] = ...
+%     r_mc_tree (tree, alpha, n_mc, level, options)
+% -------------------------------------------------------------
+%
+% Calculates the R value in a given tree using a Monte Carlo approach. 
+% It is possible to calculate the R value for all the points in the tree, 
+% only for branch points and termination points or only for branch points 
+% or only for termination points. By default, a volume correction is applied 
+% to prevent the R value from being positively biased. It is also possible 
+% to calculate confidence intervals for the R value.
+%
+%
+%
+% Inputs
+% ------
+% - tree     ::structured tree
+% - alpha    ::value	  : shrink factor used to obtain the volume 
+%		supporting a given point cloud. alpha is a scalar between 0 and 1. 
+%		Setting alpha to 0 gives the convex hull, and setting alpha to 1 
+%		gives a compact boundary that envelops the points
+%     	{DEFAULT: 0.5}
+% - n_mc     :: integer   : maximum number of Monte Carlo iterations
+%     	{DEFAULT: 100}
+% - level    ::value	  : confidence intervals are obtained with 
+%		confidence level (1 - level)
+%     	{DEFAULT: 0.05}
+% - options  ::string:
+%     	'-nv' : no volume correction
+%     	'-c' : compute confidence intervals
+%     	'-bt' : R value for branch points and termination points 
+%     	'-b' : R value for branch points 
+%     	'-t' : R value for termination points 
+%     	'-2d' : 2D tree
+%     	{DEFAULT: ''}
+%
+% Output
+% ------
+% - R:: R value (r0/rE) of the points of interest (all points in the tree, only 
+%		branch and termination points, only branch points or only termination points)
+% - Rmin:: lower bound of the confidence interval for R (r0/rEmax)
+% - Rmax:: upper bound of the confidence interval for R (r0/rEmin)     
+% - r0:: observed average nearest neighbor distance between the points        	
+% - rE:: expected average nearest neighbor distance under the assumption 
+%		of a uniform random distribution (estimated via Monte Carlo)        	
+% - rEmin:: lower bound of the confidence interval for rE
+% - rEmax:: upper bound of the confidence interval for rE       
+% - rEstd:: standard deviation of the rE values obtained in the n_mc Monte Carlo 
+%		iterations       
+% - n:: number of analyzed points to obtain R        
+% - rEs:: average nearest neighbor distances under the assumption 
+%		of a uniform random distribution in the n_mc Monte Carlo iterations        	
+%
+% Example
+% -------
+% r_mc_tree (sample_tree);
+%
+% See also PP_generator_tree
+% Uses
+%
+% Contributed by Laura Anton
+%
+% the TREES toolbox: edit, generate, visualise and analyse neuronal trees
+% Copyright (C) 2009 - 2018  Hermann Cuntz
+
+
+function [R, Rmin, Rmax, r0, rE, rEmin, rEmax, rEstd, n, rEs] = ...
+    r_mc_tree (tree, alpha, n_mc, level, options)
+
+if nargin    < 2 || isempty (alpha)
+    alpha    = 0.5;
+end
+
+if nargin    < 3 || isempty (n_mc)
+    n_mc     = 100;
+end
+
+if nargin    < 4 || isempty (level)
+    level    = 0.05;
+end
+
+if nargin    < 5  || isempty (options)
+    options  = '';
+end
+
+if contains (options, '-nv') % no volume correction
+    volcorrect = false; 
+else
+    volcorrect = true;
+end
+
+if contains (options, '-c') % compute confidence intervals
+    confintervals = true; 
+else
+    confintervals = false;
+end
+
+X            = tree.X;
+Y            = tree.Y;
+Z            = tree.Z;
+if contains  (options, '-bt')
+    idx      = find (B_tree (tree) | T_tree (tree));
+elseif contains (options, '-b')
+    idx      = find (B_tree (tree));
+elseif contains (options, '-t')
+    idx      = find (T_tree (tree));
+else
+    idx      = 1 : numel (X);
+end
+% coordinates of the points to compute R
+X            = X (idx);
+Y            = Y (idx);
+Z            = Z (idx);
+n            = numel (idx);
+bb           = [ ...
+    (min (X)) (max (X)); ...
+    (min (Y)) (max (Y)); ...
+    (min (Z)) (max (Z))];
+
+%bdRef: vector of point indices representing a single conforming 2D boundary around the points X Y 
+%	or triangulation representing a single conforming 3D boundary around the points X Y Z
+%vRef: area (2D) or volume (3D) which boundary bdRef encloses
+if contains (options, '-2d')
+    [bdRef, vRef] = boundary(X, Y, alpha);
+    XYZ      = [X, Y];
+    is2d     = 1;
+else
+    [bdRef, vRef] = boundary(X, Y, Z, alpha);
+    XYZ      = [X, Y, Z];
+    is2d     = 0;
+end
+
+% r0 from tree
+[~, d]           = knnsearch (XYZ, XYZ, 'k', 2);
+distsRef         = d (:, 2)';
+r0               = mean (distsRef);
+
+% estimate rE via mc
+n_bs             = 1000;
+rEs              = zeros (n_mc, 1);
+rEcis            = nan   (n_mc, 2);
+pts              = random_in_boundary (X, Y, Z, ...
+    bb, bdRef, vRef, n * n_mc, is2d);
+for counter      = 1 : n_mc
+    s            = (counter - 1) * n + 1 : counter * n;
+    p            = pts (s, :);
+    if volcorrect % volume correction
+        if is2d
+            [~, vMc] = boundary (p (:, 1), p (:, 2), alpha);
+            volscale = sqrt(vRef / vMc);
+        else
+            [~, vMc] = boundary (p (:, 1), p (:, 2), p (:, 3), alpha);
+            volscale = (vRef / vMc) ^ (1 / 3);
+        end
+        p        = p * volscale;
+    end
+    [~, d]       = knnsearch (p, p, 'k', 2);
+    mu           = mean (d (:, 2));
+    rEs (counter) = mu;
+    if confintervals % confidence intervals
+        muci     = bootci (n_bs, {@mean, d(:, 2)}, 'alpha', level);
+        rEcis (counter, :) = muci;
+    end
+end
+rE               = mean (rEs);
+rEstd            = std  (rEs);
+rEmin            = mean (rEcis (:, 1));
+rEmax            = mean (rEcis (:, 2));
+R                = r0 / rE;
+Rmin             = r0 / rEmax;
+Rmax             = r0 / rEmin;
+
+    function points  = random_in_boundary (X, Y, Z, bb, bd, v, n, is2d)
+        if is2d
+            bbvol    = prod (bb (1 : 2, 2) - bb (1 : 2, 1));
+            bdX      = X (bd);
+            bdY      = Y (bd);
+        else
+            bbvol    = prod (bb (:, 2)     - bb (:, 1));
+            XYZ      = [X, Y, Z];
+            fv       = struct ('vertices', XYZ, 'faces', bd);
+        end
+        volfrac      = bbvol / v;
+        points       = [];
+        while (size (points, 1) < n)
+            if size (points, 1) == 0
+                ntilde = max (round (n * volfrac * 1.2), 1);
+            else
+                ntilde = max (round (n * volfrac * 0.1), 1);
+            end
+            pX       = bb (1, 1) + ...
+                rand (ntilde, 1) * (bb (1, 2) - bb (1, 1));
+            pY       = bb (2, 1) + ...
+                rand (ntilde, 1) * (bb (2, 2) - bb (2, 1));
+            if is2d
+                pXYZ = [pX, pY];
+                ds   = p_poly_dist(pX, pY, bdX, bdY, true);
+                inside = ds <= 0 + (-1) * ds > 0;
+            else
+                pZ   = bb (3, 1) + ...
+                    rand (ntilde, 1) * (bb (3, 2) - bb (3, 1));
+                pXYZ = [pX, pY, pZ];
+                inside = inpolyhedron (fv, pXYZ);
+            end
+            idx      = find (inside == 1);
+            if numel (idx) + size (points, 1) > n
+                idx  = idx (1 : n - size (points, 1));
+            end
+            points   = [points; (pXYZ (idx, :))];
+        end
+    end
+end
+
+
+
