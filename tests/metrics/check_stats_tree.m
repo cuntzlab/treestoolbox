d1a3936f9fcc824325d69e84b65c38632b5d2f57
Bhavana Adiraju <bhavana.adi98@gmail.com>
2024-07-11 20:16:18 +0200

    check_stats_tree completed script

--- tests/metrics/check_stats_tree.m
+++ tests/metrics/check_stats_tree.m
@@ -1,13 +1,24 @@
 %% check_stats_tree
 
-%% test 1
-
-
-
+%% test 1 
+tree             = sample_tree;
+stats_tree       (tree, [], [], '-s');
+tprint           ('./panels/stats_tree1', ...
+    '-jpg -HR',                [10 10]);
 
 %% test 2
-
-
-
-tprint           ('./panels/_tree1', ...
+trees            = {sample_tree, sample2_tree};
+g                = {'sample'};
+stats_tree       (trees, g, [], '-s -dim2');
+tprint           ('./panels/stats_tree2', ...
     '-jpg -HR',                [10 10]);
+
+%% test 3
+trees            = {{sample_tree, sample2_tree}, {hss_tree, hsn_tree}};
+g                = {'sample', 'hss'};
+test_path       = './panels/test_stats.m';   
+stats_tree       (trees, g, test_path, '-s -f');
+
+tprint           ('./panels/stats_tree3', ...
+    '-jpg -HR',                [10 10]);
+
