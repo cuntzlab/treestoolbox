05459b2b3ccdfd58b6a26a736730fc3ffdd978dd
Bhavana Adiraju <bhavana.adiraju@ad.aesthetics.mpg.de>
2024-09-05 16:25:52 +0200

    check graphtheory

--- tests/edit/check_edit.m
+++ tests/edit/check_edit.m
@@ -1,16 +1,15 @@
 %% check edit folder
 
-check_cat_tree
-check_delete_tree
-check_elim0_tree
-check_elimt_tree
-check_insert_tree
-check_insertp_tree
-check_interpd_tree
-check_recon_tree
-check_repair_tree
-check_resample_tree
-check_restrain_tree
-check_root_tree
-
+check_cat_tree;
+check_delete_tree;
+check_elim0_tree;
+check_elimt_tree;
+check_insert_tree;
+check_insertp_tree;
+check_interpd_tree;
+check_recon_tree;
+check_repair_tree;
+check_resample_tree;
+check_restrain_tree;
+check_root_tree;
 
--- tests/graphtheory/check_ipar_tree.m
+++ tests/graphtheory/check_ipar_tree.m
@@ -5,23 +5,21 @@ dLPTCs           = load_tree ('dLPTCs.mtr');
 for counterN     = 1 : 100
     for counter  = 1 : length (dLPTCs{1})
         tree     = dLPTCs{1}{counter};
-        ipar_tree (tree);
+        ipar_tree (tree, [], '-s');
     end
 end
-
-%% test 2
-tree             = sample_tree;
-ipar_tree        (tree, '-s');
 tprint           ('./panels/ipar_tree1', ...
     '-jpg -HR',                [10 10]);
 
-%% test 3
-ipar_tree        (tree, '-s', T_tree (tree));
+%% test 2
+tree             = sample_tree;
+ipar_tree        (tree, '-T', '-s');
 tprint           ('./panels/ipar_tree2', ...
     '-jpg -HR',                [10 10]);
 
-
-
-
+%% test 3
+ipar_tree        (tree, [], '-s', T_tree (tree));
+tprint           ('./panels/ipar_tree2', ...
+    '-jpg -HR',                [10 10]);
 
 
